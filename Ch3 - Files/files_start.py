cebbc51bb50d9e9461263cf43abfa6011f4d9f24
Azim <ss21066@essex.ac.uk>
2022-01-23 16:15:46 +0000

    reading and writing files in python

--- Ch3 - Files/files_start.py
+++ Ch3 - Files/files_start.py
@@ -6,19 +6,27 @@
 
 def main():  
     # Open a file for writing and create it if it doesn't exist
-
+    # myfile = open("textfile.txt","w+" )
     
     # Open the file for appending text to the end
+    # myfile = open("textfile.txt","a+" )
 
 
-    # write some lines of data to the file
-
+    # # write some lines of data to the file
+    # for i in range(10):
+    #     myfile.write("This is some new text\n")
     
-    # close the file when done
-
+    # # close the file when done
+    # myfile.close()
     
     # Open the file back up and read the contents
-
+    myfile = open("textfile.txt","r" )
+    if myfile.mode == "r":
+        # contents = myfile.read()
+        # print(contents)
+        fl = myfile.readlines()
+        for x in fl:
+            print(x)
     
 if __name__ == "__main__":
     main()
