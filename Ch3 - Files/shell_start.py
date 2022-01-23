075f6cc5bcf0f64b77ecb853ffee2b73fb163da4
Azim <ss21066@essex.ac.uk>
2022-01-23 18:01:03 +0000

    Using file system and shell methods in python

--- Ch3 - Files/shell_start.py
+++ Ch3 - Files/shell_start.py
@@ -5,19 +5,29 @@
 
 import os
 from os import path
+import shutil
+from shutil import make_archive
+from zipfile import ZipFile
 
 def main():
     # make a duplicate of an existing file
-    if path.exists("textfile.txt"):
+    if path.exists("textfile.txt.bak"):
         # get the path to the file in the current directory
-        
-        # let's make a backup copy by appending "bak" to the name
-        
-        # rename the original file
-        
-        # now put things into a ZIP archive
+        src = path.realpath("textfile.txt")
 
+        # let's make a backup copy by appending "bak" to the name
+        # dst = src + ".bak"
+        # shutil.copy(src,dst)
+        # rename the original file
+        # os.rename("textfile.txt", "newfile.txt")
+
+        # now put things into a ZIP archive
+        # root_dir , tail =path.split(src)
+        # shutil.make_archive("archive", "zip", root_dir)
         # more fine-grained control over ZIP files
+        with ZipFile("testzip.zip", "w") as newzip:
+            newzip.write("newfile.txt")
+            newzip.write("newfile.txt.bak")
 
       
 if __name__ == "__main__":
